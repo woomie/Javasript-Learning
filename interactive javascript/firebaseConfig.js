e424a65c9360d7e2e5259cacf140484321bdc77a
Wunmi <wunmilawal24@gmail.com>
2025-01-30 15:40:17 -0700

    commit

---
+++ interactive javascript/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ9knWAdWhyJdrixz-QSPjckE-wXroMWA",
+    authDomain: "card-tutorial.firebaseapp.com",
+    databaseURL: "https://card-tutorial-default-rtdb.firebaseio.com",
+    projectId: "card-tutorial",
+    storageBucket: "card-tutorial.appspot.com",
+    messagingSenderId: "60677329994",
+    appId: "1:60677329994:web:b71649cbc9f6dc2a0942b3",
+  };
+
+  firebase.initializeApp(firebaseConfig);
